b8106760dfda05f6bb62ae4b46a6b65a097d0678
PotatoCannon11 <tstiglitzh@gmail.com>
2025-12-31 21:07:04 -0500

    Initial Commit

---
+++ TripleJumpAI/TripleJumpAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripleJumpAIApp.swift
+//  TripleJumpAI
+//
+//  Created by Henry Stiglitz on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct TripleJumpAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
